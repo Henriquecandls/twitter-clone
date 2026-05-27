c48a619bf6c10f9d45af58f3b5f40a63e6b0df25
Henrique Candelária <henriqueccandelaria@gmail.com>
2026-05-27 22:56:57 +0100

    Update users.js

--- Backend/app/routes/users.js
+++ Backend/app/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const usersController = require('../controllers/usersController');
 const { authenticateTokenFromHeaders, requireAdmin } = require('../middleware/auth');
 
+router.get('/discover', authenticateTokenFromHeaders, usersController.discoverUsers);
 router.post('/:id/follow', authenticateTokenFromHeaders, usersController.follow);
 router.delete('/:id/follow', authenticateTokenFromHeaders, usersController.unfollow);
 router.get('/', authenticateTokenFromHeaders, requireAdmin, usersController.listUsers);
