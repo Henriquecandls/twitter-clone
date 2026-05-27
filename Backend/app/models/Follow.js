5730cd73d00fbe43a01ddf6a5164c844727d4600
Henrique Candelária <henriqueccandelaria@gmail.com>
2026-05-27 22:56:25 +0100

    Update Follow.js

--- Backend/app/models/Follow.js
+++ Backend/app/models/Follow.js
@@ -1,9 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("Follow", {}, {
-    tableName: "seguidores",    // nome da tua tabela
+  return sequelize.define("Follow", {
+    seguidor_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    seguido_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    }
+  }, {
+    tableName: "seguidores",
     timestamps: true,
     createdAt: "created_at",
-    updatedAt: false,
-    id: false
+    updatedAt: false
   });
 };
