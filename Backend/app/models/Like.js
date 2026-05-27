8d230446ae529832735d145d5710b338ec117877
Henrique Candelária <henriqueccandelaria@gmail.com>
2026-05-27 22:56:36 +0100

    Update Like.js

--- Backend/app/models/Like.js
+++ Backend/app/models/Like.js
@@ -1,9 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("Like", {}, {
+  return sequelize.define("Like", {
+    utilizador_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    tweet_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    }
+  }, {
     tableName: "likes",
     timestamps: true,
     createdAt: "created_at",
-    updatedAt: false,
-    id: false
+    updatedAt: false
   });
 };
